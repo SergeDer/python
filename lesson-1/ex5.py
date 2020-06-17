83ebdf70c2bcf197ddc058286e7611a637cfa740
Serge <proserge@gmail.com>
2020-06-17 15:44:16 +0300

    Изменения типов принимаемых данных
    Добавлена возможность принять данные типа float в 5-м и 6-м заданиях
    Добавлена проверка числа введенных сотрудников в 5-м задании

--- lesson-1/ex5.py
+++ lesson-1/ex5.py
@@ -1,13 +1,13 @@
 # Соотношение выручки и издержек
 while 1:
-    income = int(input('Введите выручку: '))
+    income = float(input('Введите выручку: '))
     if income <= 0:
         print('Выручка должна быть положительной!')
     else:
         break
 
 while 1:
-    outgo = int(input('Введите издержки: '))
+    outgo = float(input('Введите издержки: '))
     if outgo <= 0:
         print('Издержки должны быть положительны!')
     else:
@@ -16,8 +16,16 @@ while 1:
 if income > outgo:
     print('Поздравляем! Выручка больше издержек! Прибыль: {:.2f} Рентабельность: {:.2f}'.format(income - outgo, (
             income - outgo) / outgo))
-    number_of_emp = int(input('Введите число сотрудников: '))
+
+    while 1:
+        number_of_emp = int(input('Введите число сотрудников: '))
+        if number_of_emp <= 0:
+            print('Количество сотрудников должно быть больше нуля!')
+        else:
+            break
+
     print('Прибыль на одного сотрудника: {:.2f}'.format((income - outgo) / number_of_emp))
+
 elif income == outgo:
     print('Выручка равна издержкам. Вы работаете а ноль!')
 else:
